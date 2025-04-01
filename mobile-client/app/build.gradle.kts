dff5223de4c9c2d3a20eee5af29c9e7d7825148f
Walkername <05Timur51@gmail.com>
2025-04-01 16:35:31 +0300

    feat(mqtt): added mqtt connection

--- mobile-client/app/build.gradle.kts
+++ mobile-client/app/build.gradle.kts
@@ -49,6 +49,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    val mqttVersion = "4.3";
+    implementation("com.github.hannesa2:paho.mqtt.android:$mqttVersion")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ mobile-client/app/src/main/java/ru/locationwatch/mobile_client/AppConfig.kt
@@ -0,0 +1,18 @@
+package ru.locationwatch.mobile_client
+
+import android.content.Context
+import java.util.Properties
+
+class AppConfig(private val context: Context) {
+
+    private val properties: Properties by lazy {
+        Properties().apply {
+            context.assets.open("mqtt_config.properties").use { load(it) }
+        }
+    }
+
+    val serverURI: String get() = properties.getProperty("mqtt.server.uri")
+    val userId: String get() = properties.getProperty("mqtt.user.id")
+    val userPassword: String get() = properties.getProperty("mqtt.user.password")
+
+}
