3438b15c71aa0e2b9b9303291b30eee18fb29e39
Walkername <05Timur51@gmail.com>
2025-05-04 11:57:36 +0300

    feat(validation): added validation annotations to Zone and ZoneDTO

--- backend/src/main/java/ru/locationwatch/backend/dto/ZoneDTO.java
+++ backend/src/main/java/ru/locationwatch/backend/dto/ZoneDTO.java
@@ -1,13 +1,19 @@
 package ru.locationwatch.backend.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import ru.locationwatch.backend.models.Coordinate;
 
 import java.util.List;
 
 public class ZoneDTO {
 
+    @NotEmpty(message = "Type name should no be empty")
+    @Size(max = 100, message = "Type should be less than 100")
     private String typeName;
 
+    // Maybe to find out what annotations
+    // can I use to validate collections
     private List<Coordinate> area;
 
     public ZoneDTO() {}
