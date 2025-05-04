bf500fa7a81af33b715773ea9a9e8e3818914f2d
Walkername <05Timur51@gmail.com>
2025-05-04 11:51:56 +0300

    feat(validation): added validation annonations to person

--- backend/src/main/java/ru/locationwatch/backend/models/Person.java
+++ backend/src/main/java/ru/locationwatch/backend/models/Person.java
@@ -1,6 +1,8 @@
 package ru.locationwatch.backend.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity(name = "person")
 public class Person {
@@ -10,9 +12,12 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotEmpty(message = "Username should not be empty")
+    @Size(min = 1, max = 20, message = "Username size should be greater than 1 and less than 20")
     @Column(name = "username")
     private String username;
 
+    @NotEmpty(message = "Password should not be empty")
     @Column(name = "password")
     private String password;
 
