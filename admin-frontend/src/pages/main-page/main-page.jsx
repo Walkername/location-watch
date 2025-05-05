d8b7911cdff27b37ac0cf4b98b39742a7851d905
Walkername <05Timur51@gmail.com>
2025-05-05 21:20:50 +0300

    feat(routes): initial main page

---
+++ admin-frontend/src/pages/main-page/main-page.jsx
@@ -0,0 +1,17 @@
+
+
+function MainPage() {
+
+    return (
+        <>
+            <h1>Admin Interface</h1>
+            <div className="page-content-container">
+                <div className="page-content">
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default MainPage;
