325207421571dd80a5c6d04c1455d79104fe82c3
Walkername <05Timur51@gmail.com>
2025-05-10 19:03:50 +0300

    feat(mobile-client): navigation to main screen when login is successed; some code refactoring

--- mobile-client/app/src/main/java/ru/locationwatch/mobile_client/ui/AuthViewModel.kt
+++ mobile-client/app/src/main/java/ru/locationwatch/mobile_client/ui/AuthViewModel.kt
@@ -1,8 +1,7 @@
-package ru.locationwatch.mobile_client.ui.theme.ui
+package ru.locationwatch.mobile_client.ui
 
 import android.app.Application
 import android.util.Log
-import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
