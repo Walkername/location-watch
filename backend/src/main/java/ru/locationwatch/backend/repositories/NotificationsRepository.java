306d32ba95425d1285e9a7c819cc7822d9b004dc
Walkername <05Timur51@gmail.com>
2025-06-06 19:21:12 +0300

    feat(backend): send notification to mobile client when user is inside the restricted zone

--- backend/src/main/java/ru/locationwatch/backend/repositories/NotificationsRepository.java
+++ backend/src/main/java/ru/locationwatch/backend/repositories/NotificationsRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.locationwatch.backend.models.FirebaseToken;
 
+import java.util.Optional;
+
 @Repository
 public interface NotificationsRepository extends JpaRepository<FirebaseToken, Integer> {
 
     void deleteByPersonIdAndToken(int personId, String token);
+
+    Optional<FirebaseToken> findByPersonId(int personId);
 
 }
