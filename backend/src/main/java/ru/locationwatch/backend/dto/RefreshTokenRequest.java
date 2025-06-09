446faf46babde158330ca07239a4a811a2c383c1
Walkername <05Timur51@gmail.com>
2025-06-09 20:19:35 +0300

    feat(backend): endpoints and logic to refresh a pair of tokens (access, refresh)

---
+++ backend/src/main/java/ru/locationwatch/backend/dto/RefreshTokenRequest.java
@@ -0,0 +1,18 @@
+package ru.locationwatch.backend.dto;
+
+public class RefreshTokenRequest {
+
+    private String refreshToken;
+
+    public RefreshTokenRequest(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
---
+++ backend/src/main/java/ru/locationwatch/backend/util/RefreshException.java
@@ -0,0 +1,9 @@
+package ru.locationwatch.backend.util;
+
+public class RefreshException extends RuntimeException {
+
+    public RefreshException(String message) {
+        super(message);
+    }
+
+}
