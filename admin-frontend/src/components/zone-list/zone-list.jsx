2164f63aa17853a6c65f4f2c8b59f57682972308
Walkername <05Timur51@gmail.com>
2025-06-13 22:15:52 +0300

    feat(frontend): added scroll to zones and violations lists

--- admin-frontend/src/components/zone-list/zone-list.jsx
+++ admin-frontend/src/components/zone-list/zone-list.jsx
@@ -4,32 +4,34 @@ function ZoneList({ zones, onZoneClick }) {
         <div class="list-section">
             <h2>All Zones</h2>
             <div className="zones-container" id="zonesContainer">
-                {zones.map(zone => (
-                    <div
-                        key={zone.id}
-                        className="zone-card"
-                        onClick={() => onZoneClick(zone.id)}
-                    >
-                        <div className="zone-header">
-                            <div className="zone-title">{zone.title}</div>
-                            <div className={`zone-badge ${zone.typeName === 'NO_SPEED' ? 'badge-no' :
-                                zone.typeName === 'LESS_SPEED' ? 'badge-less' : ''
-                                }`}>
-                                {zone.typeName}
+                <div className="zones-list-scroll">
+                    {zones.map(zone => (
+                        <div
+                            key={zone.id}
+                            className="zone-card"
+                            onClick={() => onZoneClick(zone.id)}
+                        >
+                            <div className="zone-header">
+                                <div className="zone-title">{zone.title}</div>
+                                <div className={`zone-badge ${zone.typeName === 'NO_SPEED' ? 'badge-no' :
+                                    zone.typeName === 'LESS_SPEED' ? 'badge-less' : ''
+                                    }`}>
+                                    {zone.typeName}
+                                </div>
                             </div>
-                        </div>
-                        <div className="zone-details">
-                            {
-                                zone.typeName === "LESS_SPEED" ? (
-                                    <span><strong>Max Speed:</strong> {zone.speed} km/h</span>
-                                ) 
-                                : <span>It is prohibited to be in this area</span>
-                            }
+                            <div className="zone-details">
+                                {
+                                    zone.typeName === "LESS_SPEED" ? (
+                                        <span><strong>Max Speed:</strong> {zone.speed} km/h</span>
+                                    )
+                                        : <span>It is prohibited to be in this area</span>
+                                }
+
+                            </div>
 
                         </div>
-
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     )
