7117069d585cc149ac2e5e736046c5558d17fecc
Walkername <05Timur51@gmail.com>
2025-06-13 22:15:52 +0300

    feat(frontend): added scroll to zones and violations lists

--- admin-frontend/src/components/violators-list/violators-list.jsx
+++ admin-frontend/src/components/violators-list/violators-list.jsx
@@ -4,39 +4,85 @@ import validateDate from "../../utils/date-validation/date-validation";
 
 function ViolatorsList({ violations }) {
     return (
-        <div class="list-section">
+        <div className="list-section">
             <h2>Active Violations</h2>
-            <ul class="list">
-                {
-                    Array.from(violations.entries()).map(([key, value], index) => (
-                        <div key={index} class="violation-card">
-                            <div class="violation-header">
-                                <div class="violation-id">User #{key}</div>
-                                <div class="violation-time">{validateDate(value.timestamp)}</div>
-                            </div>
-                            <div class="violation-body">
-                                <span><strong>Zones:</strong>
-                                    {
-                                        value.crossedZones.map((zone, index) => {
-                                            return <span key={index}> {zone.title} </span>
-                                        })
-                                    }
-                                </span>
-                                <span><strong>Speed:</strong> {value.speed} km/h</span>
-                            </div>
-                        </div>
-                    ))
-                }
+            <ul className="list">
+                <div className="violations-container" id="violationsContainer">
+                    <div className="violations-list-scroll">
+                        {
+                            Array.from(violations.entries()).map(([key, value], index) => (
+                                <div key={index} className="violation-card">
+                                    <div className="violation-header">
+                                        <div className="violation-id">User #{key}</div>
+                                        <div className="violation-time">{validateDate(value.timestamp)}</div>
+                                    </div>
+                                    <div className="violation-body">
+                                        <span><strong>Zones:</strong>
+                                            {
+                                                value.crossedZones.map((zone, index) => {
+                                                    return <span key={index}> {zone.title} </span>
+                                                })
+                                            }
+                                        </span>
+                                        <span><strong>Speed:</strong> {value.speed} km/h</span>
+                                    </div>
+                                </div>
+                            ))
+                        }
 
-                <div class="violations-container" id="violationsContainer">
-                    <div class="violation-card">
-                        <div class="violation-header">
-                            <div class="violation-id">Test User</div>
-                            <div class="violation-time">01.02.2002, 14:36:03</div>
+                        <div className="violation-card">
+                            <div className="violation-header">
+                                <div className="violation-id">Test User</div>
+                                <div className="violation-time">01.02.2002, 14:36:03</div>
+                            </div>
+                            <div className="violation-body">
+                                <span><strong>Zones:</strong> Zone A, Zone B</span>
+                                <span><strong>Speed:</strong> 75 km/h</span>
+                            </div>
                         </div>
-                        <div class="violation-body">
-                            <span><strong>Zones:</strong> Zone A, Zone B</span>
-                            <span><strong>Speed:</strong> 75 km/h</span>
+
+                        <div className="violation-card">
+                            <div className="violation-header">
+                                <div className="violation-id">Test User</div>
+                                <div className="violation-time">01.02.2002, 14:36:03</div>
+                            </div>
+                            <div className="violation-body">
+                                <span><strong>Zones:</strong> Zone A, Zone B</span>
+                                <span><strong>Speed:</strong> 75 km/h</span>
+                            </div>
+                        </div>
+
+                        <div className="violation-card">
+                            <div className="violation-header">
+                                <div className="violation-id">Test User</div>
+                                <div className="violation-time">01.02.2002, 14:36:03</div>
+                            </div>
+                            <div className="violation-body">
+                                <span><strong>Zones:</strong> Zone A, Zone B</span>
+                                <span><strong>Speed:</strong> 75 km/h</span>
+                            </div>
+                        </div>
+
+                        <div className="violation-card">
+                            <div className="violation-header">
+                                <div className="violation-id">Test User</div>
+                                <div className="violation-time">01.02.2002, 14:36:03</div>
+                            </div>
+                            <div className="violation-body">
+                                <span><strong>Zones:</strong> Zone A, Zone B</span>
+                                <span><strong>Speed:</strong> 75 km/h</span>
+                            </div>
+                        </div>
+
+                        <div className="violation-card">
+                            <div className="violation-header">
+                                <div className="violation-id">Test User</div>
+                                <div className="violation-time">01.02.2002, 14:36:03</div>
+                            </div>
+                            <div className="violation-body">
+                                <span><strong>Zones:</strong> Zone A, Zone B</span>
+                                <span><strong>Speed:</strong> 75 km/h</span>
+                            </div>
                         </div>
                     </div>
                 </div>
